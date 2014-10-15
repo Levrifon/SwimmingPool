69e1b6a6a26d69ee415ab813a17b155a3730ec4a
Levrifon <rems-msn@hotmail.fr>
2014-10-15 12:57:22 +0200

    completed Swimmer.java

--- src/Swimmer.java
+++ src/Swimmer.java
@@ -1,4 +1,67 @@
+import pool.BasketPool;
+import pool.CubiclePool;
+
 
 public class Swimmer {
+private String name;
+private BasketPool basket;
+private CubiclePool cubicle;
+private int unwearTime,swimmingTime,wearTime;
 
+	public Swimmer(String name, BasketPool baskets, CubiclePool cubicles,
+			int i, int j, int k) {
+		this.setName(name);
+		this.basket = baskets;
+		this.cubicle = cubicles;
+		this.unwearTime = i;
+		this.swimmingTime = j;
+		this.wearTime = k;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBasket(BasketPool basket) {
+		this.basket = basket;
+	}
+
+	public CubiclePool getCubicle() {
+		return cubicle;
+	}
+
+	public void setCubicle(CubiclePool cubicle) {
+		this.cubicle = cubicle;
+	}
+
+	public int getUnwearTime() {
+		return unwearTime;
+	}
+
+	public void setUnwearTime(int unwearTime) {
+		this.unwearTime = unwearTime;
+	}
+
+	public int getSwimmingTime() {
+		return swimmingTime;
+	}
+
+	public void setSwimmingTime(int swimmingTime) {
+		this.swimmingTime = swimmingTime;
+	}
+
+	public int getWearTime() {
+		return wearTime;
+	}
+
+	public void setWearTime(int wearTime) {
+		this.wearTime = wearTime;
+	}
+	public BasketPool getBasket() {
+		return basket;
+	}
 }
